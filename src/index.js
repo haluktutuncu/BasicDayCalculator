675c4e549e75314ea3d1fa736e24b717701cac20
haluktutuncu <112336855+haluktutuncu@users.noreply.github.com>
2023-08-17 19:33:58 +0300

    v.1.1

--- src/index.js
+++ src/index.js
@@ -2,74 +2,69 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function App() {
-  const [step, setStep] = useState(0);
-  const [counter, setCounter] = useState(0);
-  const [currentDate, setCurrentDate] = useState(new Date());
-
-  const daysOfWeek = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
-  {
-    /* Button actions */
-  }
-  const handleStepBack = () => {
-    if (step >= 1) {
-      setStep(step - 1);
-    }
-  };
-
-  const handleStepNext = () => {
-    setStep(step + 1);
-  };
-  {
-    /* Button actions */
-  }
-  const handleCounterBack = () => {
-    setCounter(counter - step);
-    setCurrentDate((prevDate) => {
-      const newDate = new Date(prevDate);
-      newDate.setDate(newDate.getDate() - step);
-      return newDate;
-    });
-  };
-
-  const handleCounterNext = () => {
-    setCounter(counter + step);
-    setCurrentDate((prevDate) => {
-      const newDate = new Date(prevDate);
-      newDate.setDate(newDate.getDate() + step);
-      return newDate;
-    });
-  };
-
+export default function App() {
   return (
     <div className="App">
-      <div>
-        <button onClick={handleStepBack}>-</button>
-        <p className="num1">Step:</p> <span>{step}</span>
-        <button onClick={handleStepNext}>+</button>
-      </div>
-      <div>
-        <button onClick={handleCounterBack}>-</button>
-        <p className="num1">Count:</p> <span>{counter}</span>
-        <button onClick={handleCounterNext}>+</button>
-        <br></br>
-        <br></br>
-      </div>
-      <span className="main">{`${Math.abs(counter)} days from today is ${
-        daysOfWeek[currentDate.getDay()]
-      }`}</span>{" "}
-      <p>{currentDate.toLocaleDateString()}</p>
+      <Counter />
     </div>
   );
 }
 
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  const date = new Date(new Date());
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div className="container">
+      <div className="">
+        <input
+          className="range"
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>Step: {step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          className="text-box"
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p className="date">
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(<App />);
